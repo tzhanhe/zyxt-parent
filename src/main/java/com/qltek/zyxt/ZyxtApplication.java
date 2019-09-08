d0766f49bb6d7597a9d6e0a82f10a54efadd7f52
tzhanhe <398817956@qq.com>
2019-09-08 17:35:42 +0800

    init

---
+++ src/main/java/com/qltek/zyxt/ZyxtApplication.java
@@ -0,0 +1,13 @@
+package com.qltek.zyxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyxtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZyxtApplication.class, args);
+    }
+
+}
---
+++ zyxt-provider/src/test/java/com/qltek/zyxtprovider/ZyxtProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qltek.zyxtprovider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZyxtProviderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
